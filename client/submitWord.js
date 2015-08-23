81396bba26acf51634cf64c3e8c709e2954deebb
Teck <teck@tekleemedia.com>
2015-08-23 16:08:40 -0700

    packery and draggabilly added

--- client/submitWord.js
+++ client/submitWord.js
@@ -1,9 +1,33 @@
+
+
 Template.submitWord.events({
 	'submit .submitWordForm': function (e) {
+		var pckry = function () {
+			$('.list-group').packery({
+				itemSelector: '.list-group-item',
+				gutter: 5,
+				"rowHeight": 60
+			});
+		}
+		var draggability = function() {
+			$('.list-group').find('.list-group-item').each( function( i, itemElem ) {
+			// make element draggable with Draggabilly
+			var draggie = new Draggabilly( itemElem );
+			// bind Draggabilly events to Packery
+			$('.list-group').packery( 'bindDraggabillyEvents', draggie );
+			});
+		}
 		e.preventDefault();
-
 		var word = {word:e.target.submitWord.value};
-		console.log(word)
+		console.log(word);
 		Meteor.call('wordInsert', word);
+		setTimeout(function() {
+			draggability();
+			$('.list-group').packery();
+			var itemnum = $('.list-group').find('.list-group-item').length;
+			console.log($('.list-group').find('.list-group-item')[itemnum-1]);
+			console.log('packary');
+		}, 1000);
+
 	}
 });
--- client/wordBank.js
+++ client/wordBank.js
@@ -1,15 +1,37 @@
+var packery = function () {
+	$('.list-group').packery({
+		itemSelector: '.list-group-item',
+		gutter: 5
+	});
+}
+var draggability = function() {
+	$('.list-group').find('.list-group-item').each( function( i, itemElem ) {
+	// make element draggable with Draggabilly
+	var draggie = new Draggabilly( itemElem );
+	// bind Draggabilly events to Packery
+	$('.list-group').packery( 'bindDraggabillyEvents', draggie );
+	});
+}
+
+
 Template.wordBank.helpers({
 	words: function() {
 		return Words.find();
 	}
 });
 
+
+Template.wordBank.onRendered(function() {
+	packery();
+	draggability();
+});
+
+
+
 Template.wordBank.events({
 	'click .wordItem': function (e) {
 		e.preventDefault();
 	var word = ($(".wordItem").text());
-
 	console.log(word)
 	}
-
 });
