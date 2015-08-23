b3a1594f01e3e3a870d2ec3fd146fbedf32018e9
Spacemixup <xchrislee@gmail.com>
2015-08-22 18:08:57 -0700

    Word Card logic partial

---
+++ server/server.js
@@ -0,0 +1,12 @@
+Meteor.methods({
+	wordInsert: function(postAttributes) {
+		var user = Meteor.user(); 
+		
+		var word = _.extend(postAttributes, {
+			userId: user._id,
+			definition: "definition from api call"	
+		});
+		
+		var postId = Words.insert(word)
+	}
+})
