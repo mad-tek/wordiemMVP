5780aa0a9d328ae995c0a6c66d16a44c829bee2c
Teck <teck@tekleemedia.com>
2015-08-24 16:29:18 -0700

    updated word cards with definition calls to db

--- client/wordBank.js
+++ client/wordBank.js
@@ -17,6 +17,9 @@ var draggability = function() {
 Template.wordBank.helpers({
 	words: function() {
 		return Words.find();
+	},
+	definition: function (){
+		return Words.find({word: Session.get("selectedWord")});
 	}
 });
 
@@ -32,19 +35,29 @@ Template.wordBank.events({
 	'click .wordItem': function (e) {
 		e.preventDefault();
 		var word = ($(".wordItem").text());
+		Session.set('selectedWord',this.word);
 
 		//one card open at a time
 		$('.list-group').find('.gigante').each( function( i, itemElem ) {
 			$(itemElem).toggleClass('gigante');
 		});
+		$('.definition-ondemand').remove();
 
 		//open current word card
 		$(e.target).parentsUntil('li.list-group-item').toggleClass('gigante');
 		$('.list-group').packery();
 
+
 		//populates word card
-		var word = Words.find({word: Session.get("selectedWord")});
-		console.log(e.target.parentNode);
+		var word = Words.find({word: Session.get("selectedWord")}, {definition: 1, word: 1, _id:0});
+		console.log(word.map(function(i){return i.definition;})[0]);
+		var node = document.createElement('p');
+		var titlenode = document.createTextNode(word.map(function(query) {return query.word;})[0] + ": ");
+		var textnode = document.createTextNode(word.map(function(query){return query.definition;})[0]);
+		node.className = "definition-ondemand";
+		node.appendChild(titlenode);
+		node.appendChild(textnode);
+		e.target.parentNode.appendChild(node);
 	},
 	'mousedown .delete-item, dblclick .wordItem': function(e) {
 		e.preventDefault;
