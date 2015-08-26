a5b3baef23a0332760ce84b2ad7887c67250a6ac
Teck <teck@tekleemedia.com>
2015-08-25 22:29:06 -0700

    fixed word card open bug

--- client/wordBank.js
+++ client/wordBank.js
@@ -12,6 +12,28 @@ var draggability = function() {
 	$('.list-group').packery( 'bindDraggabillyEvents', draggie );
 	});
 }
+var cardHandler = function(wordcard) {
+	//open current word card
+	$(wordcard).parent().toggleClass('gigante');
+	$('.list-group').packery();
+
+	//populates word card
+	var word = Words.find({word: Session.get("selectedWord")}, {definition: 1, word: 1, _id:0});
+	var node = document.createElement('p');
+	var titlenode = document.createTextNode(word.map(function(query) {return query.word;})[0] + ": ");
+	var textnode = document.createTextNode(word.map(function(query){return query.definition;})[0]);
+	node.className = "definition-ondemand";
+	node.appendChild(titlenode);
+	node.appendChild(textnode);
+	wordcard.parentNode.appendChild(node);
+
+	//delete word button
+	var closediv = document.createElement('a');
+	var closebutton = document.createTextNode("delete this word");
+	closediv.className = "close-button";
+	closediv.appendChild(closebutton);
+	wordcard.parentNode.appendChild(closediv);
+}
 
 
 Template.wordBank.helpers({
@@ -32,7 +54,7 @@ Template.wordBank.onRendered(function() {
 
 
 Template.wordBank.events({
-	'click .wordItem': function (e) {
+	'click .wordItem, click .list-group-item': function (e) {
 		e.preventDefault();
 		var word = ($(".wordItem").text());
 		Session.set('selectedWord',this.word);
@@ -44,26 +66,16 @@ Template.wordBank.events({
 		$('.definition-ondemand').remove();
 		$('.close-button').remove();
 
-		//open current word card
-		$(e.target).parentsUntil('li.list-group-item').toggleClass('gigante');
-		$('.list-group').packery();
+		//click on the link and clicking on the link cell
+		var target2 = e.target.getElementsByTagName('a')[0];
+		var target1 = e.target;
+		if(e.target.getElementsByTagName('a').length === 0){
+			cardHandler(target1);
+		}
+		if(e.target.getElementsByTagName('a').length === 1){
+			cardHandler(target2);
+		}
 
-
-		//populates word card
-		var word = Words.find({word: Session.get("selectedWord")}, {definition: 1, word: 1, _id:0});
-		var node = document.createElement('p');
-		var titlenode = document.createTextNode(word.map(function(query) {return query.word;})[0] + ": ");
-		var textnode = document.createTextNode(word.map(function(query){return query.definition;})[0]);
-		node.className = "definition-ondemand";
-		node.appendChild(titlenode);
-		node.appendChild(textnode);
-		e.target.parentNode.appendChild(node);
-		//delete button
-		var closediv = document.createElement('a');
-		var closebutton = document.createTextNode("delete this word");
-		closediv.className = "close-button";
-		closediv.appendChild(closebutton);
-		e.target.parentNode.appendChild(closediv);
 	},
 	'mousedown .close-button, click .close-button': function(e) {
 		e.preventDefault;
